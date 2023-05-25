c00db1f64a6c431e9778e788a9be8421afd4df06
Ame0304 <rhj903265106@gmail.com>
2023-05-25 17:24:38 -0400

    Replaced cats content and pictures with dogs

--- searchfromindex.js
+++ searchfromindex.js
@@ -1,120 +1,158 @@
 $(document).ready(function () {
-    //receiving the data from the index page
-    const json = localStorage.getItem('sForm');
-    const obj = JSON.parse(json);
+  //receiving the data from the index page
+  const json = localStorage.getItem("sForm");
+  const obj = JSON.parse(json);
 
-    if (obj.catname != '') {
-        const url = `https://api.thecatapi.com/v1/breeds`;
-        const api_key = "DEMO_API_KEY"
+  if (obj.catname != "") {
+    const url = `https://api.thedogapi.com/v1/breeds`;
+    const api_key = "DEMO_API_KEY";
 
-        var storedBreeds = []
+    var storedBreeds = [];
 
-        fetch(url, {
-            headers: {
-                'x-api-key': api_key
-            }
-        })
+    fetch(url, {
+      headers: {
+        "x-api-key": api_key,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
 
-            .then((response) => {
-                return response.json();
-            })
+      .then((data) => {
+        //filter to only include those with an `image` object
+        data = data.filter((img) => img.image?.url != null);
 
-            .then((data) => {
+        storedBreeds = data; /* receive all the cats data */
 
-                //filter to only include those with an `image` object
-                data = data.filter(img => img.image?.url != null)
+        sValue(storedBreeds); //call the function to create the gallery
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-                storedBreeds = data; /* receive all the cats data */
+  function sValue(datas) {
+    console.log("executei funcao svalue");
+    const principal = $("#gallery");
+    $(principal).html("");
 
-                sValue(storedBreeds); //call the function to create the gallery
+    console.log(datas);
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+    let filteredBreeds = [];
+    console.log(datas.length);
+    for (let j = 0; j < datas.length; j++) {
+      if (datas[j].name.toLowerCase().includes(obj.catname)) {
+        filteredBreeds.push(j); //save the data accordingly with the search
+      }
     }
 
-    function sValue(datas) {
-        console.log("executei funcao svalue")
-        const principal = $("#gallery");
-        $(principal).html('');
+    displayValues(filteredBreeds); //call the function to display only the data related to the input
+  }
 
-        console.log(datas)
+  function displayValues(datas) {
+    console.log("executei displayvalues");
+    console.log(datas.length);
+    for (let j = 0; j < datas.length; j++) {
+      const principal = $("#gallery");
+      console.log("entrei no for");
+      //creating the elements to populate the gallery
+      const div = $("<div />", { id: "container" });
 
-        let filteredBreeds = [];
-        console.log(datas.length)
-        for (let j = 0; j < datas.length; j++) {
-            if (datas[j].name.toLowerCase().includes(obj.catname)) {
-                filteredBreeds.push(j); //save the data accordingly with the search
-            }
-        }
+      const image = $("<img />", {
+        id: "breed_image",
+        src: storedBreeds[datas[j]].image.url,
+      });
 
-        displayValues(filteredBreeds); //call the function to display only the data related to the input
+      const breedName = $("<h2 />", {
+        id: "breed_name",
+        class: "show",
+        html: storedBreeds[datas[j]].name,
+      });
+
+      const temperament = $("<p />", {
+        id: "temperament",
+        class: "show",
+        html: storedBreeds[datas[j]].temperament,
+      });
+
+      const description = $("<p />", {
+        id: "description",
+        class: "hidden",
+        html: storedBreeds[datas[j]].description,
+      });
+
+      const url = $("<a />", {
+        id: "url",
+        class: "hidden",
+        target: "_blank",
+        html: "Learn More",
+        href: storedBreeds[datas[j]].wikipedia_url,
+      });
+
+      const affection = $("<p />", {
+        id: "affection",
+        class: "hidden",
+        html: "Affection Level: ",
+      });
+
+      const energy = $("<p />", {
+        id: "energy",
+        class: "hidden",
+        html: "Energy Level: ",
+      });
+
+      const dog = $("<p />", {
+        id: "dog",
+        class: "hidden",
+        html: "Dog Friendly: ",
+      });
+
+      const stranger = $("<p />", {
+        id: "stranger",
+        class: "hidden",
+        html: "Stranger Friendly: ",
+      });
+
+      const intelligence = $("<p />", {
+        id: "intelligence",
+        class: "hidden",
+        html: "Intelligence Level: ",
+      });
+
+      for (i = 1; i <= storedBreeds[datas[j]].affection_level; i++) {
+        $(affection).append("⭐"); //add star accordingly with the data
+      }
+
+      for (i = 1; i <= storedBreeds[datas[j]].energy_level; i++) {
+        $(energy).append("⭐");
+      }
+
+      for (i = 1; i <= storedBreeds[datas[j]].dog_friendly; i++) {
+        $(dog).append("⭐");
+      }
+
+      for (i = 1; i <= storedBreeds[datas[j]].stranger_friendly; i++) {
+        $(stranger).append("⭐");
+      }
+
+      for (i = 1; i <= storedBreeds[datas[j]].intelligence; i++) {
+        $(intelligence).append("⭐");
+      }
+
+      //append the elements to container div
+      $(div).append(image);
+      $(div).append(breedName);
+      $(div).append(temperament);
+      $(div).append(description);
+      $(div).append(affection);
+      $(div).append(energy);
+      $(div).append(dog);
+      $(div).append(stranger);
+      $(div).append(intelligence);
+      $(div).append(url);
+
+      //append the container div to principal div
+      $(principal).append(div);
     }
-
-    function displayValues(datas) {
-        console.log("executei displayvalues")
-        console.log(datas.length)
-        for (let j = 0; j < datas.length; j++) {
-            const principal = $("#gallery");
-            console.log("entrei no for")
-            //creating the elements to populate the gallery
-            const div = $("<div />", { id: 'container' });
-
-            const image = $("<img />", { id: 'breed_image', src: storedBreeds[datas[j]].image.url });
-
-            const breedName = $("<h2 />", { id: 'breed_name', class: 'show', html: storedBreeds[datas[j]].name });
-
-            const temperament = $("<p />", { id: 'temperament', class: 'show', html: storedBreeds[datas[j]].temperament });
-
-            const description = $("<p />", { id: 'description', class: 'hidden', html: storedBreeds[datas[j]].description });
-
-            const url = $("<a />", { id: 'url', class: 'hidden', target: '_blank', html: 'Learn More', href: storedBreeds[datas[j]].wikipedia_url });
-
-            const affection = $("<p />", { id: 'affection', class: 'hidden', html: 'Affection Level: ' });
-
-            const energy = $("<p />", { id: 'energy', class: 'hidden', html: 'Energy Level: ' });
-
-            const dog = $("<p />", { id: 'dog', class: 'hidden', html: 'Dog Friendly: ' });
-
-            const stranger = $("<p />", { id: 'stranger', class: 'hidden', html: 'Stranger Friendly: ' });
-
-            const intelligence = $("<p />", { id: 'intelligence', class: 'hidden', html: 'Intelligence Level: ' });
-
-            for (i = 1; i <= storedBreeds[datas[j]].affection_level; i++) {
-                $(affection).append('⭐'); //add star accordingly with the data
-            }
-
-            for (i = 1; i <= storedBreeds[datas[j]].energy_level; i++) {
-                $(energy).append('⭐');
-            }
-
-            for (i = 1; i <= storedBreeds[datas[j]].dog_friendly; i++) {
-                $(dog).append('⭐');
-            }
-
-            for (i = 1; i <= storedBreeds[datas[j]].stranger_friendly; i++) {
-                $(stranger).append('⭐');
-            }
-
-            for (i = 1; i <= storedBreeds[datas[j]].intelligence; i++) {
-                $(intelligence).append('⭐');
-            }
-
-            //append the elements to container div
-            $(div).append(image);
-            $(div).append(breedName);
-            $(div).append(temperament);
-            $(div).append(description);
-            $(div).append(affection);
-            $(div).append(energy);
-            $(div).append(dog);
-            $(div).append(stranger);
-            $(div).append(intelligence);
-            $(div).append(url);
-
-            //append the container div to principal div
-            $(principal).append(div);
-        }
-    }
+  }
 });
--- vote.js
+++ vote.js
@@ -1,121 +1,112 @@
-const API_URL = `https://api.thecatapi.com/v1/`;
-const API_KEY = "live_OLQLaHy30gq7VRz7RPt8nO6l06F7quDfOMqfPV9UP4PnTPm3JPfvktXnhPTX4tuY";
+const API_URL = `https://api.thedogapi.com/v1/`;
+const API_KEY =
+  "live_OLQLaHy30gq7VRz7RPt8nO6l06F7quDfOMqfPV9UP4PnTPm3JPfvktXnhPTX4tuY";
 
-let id = '';
+let id = "";
 let currentImageToVoteOn;
 
-
-
 function showVoteOptions() {
-  document.getElementById("grid").innerHTML = '';
+  document.getElementById("grid").innerHTML = "";
 
-  document.getElementById('vote-options').style.display = 'block';
-  document.getElementById('vote-results').style.display = 'none';
-  document.getElementById('inner-container').style.display = 'flex';
+  document.getElementById("vote-options").style.display = "block";
+  document.getElementById("vote-results").style.display = "none";
+  document.getElementById("inner-container").style.display = "flex";
 
   showImageToVoteOn();
 }
 
 function showImageToVoteOn() {
-
   const url = `${API_URL}images/search`;
 
   fetch(url, {
     headers: {
-      'x-api-key': API_KEY
-    }
+      "x-api-key": API_KEY,
+    },
   })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       currentImageToVoteOn = data[0];
-      document.getElementById("image-to-vote-on").src = currentImageToVoteOn.url;
+      document.getElementById("image-to-vote-on").src =
+        currentImageToVoteOn.url;
 
       let img = document.getElementById("image-to-vote-on");
       if (img.style.height >= img.style.width) {
-        img.style.height = '-webkit-fill-available';
-        img.style.width = 'auto';
+        img.style.height = "-webkit-fill-available";
+        img.style.width = "auto";
       } else {
-        img.style.width = '-webkit-fill-available';
-        img.style.height = 'auto';
+        img.style.width = "-webkit-fill-available";
+        img.style.height = "auto";
       }
     });
-
 }
 
 function vote(value) {
-
   const url = `${API_URL}votes/`;
   const body = {
     image_id: currentImageToVoteOn.id,
-    value
-  }
+    value,
+  };
   fetch(url, {
-    method: "POST", body: JSON.stringify(body), headers: {
-      'content-type': "application/json",
-      'x-api-key': API_KEY
-    }
-  })
-    .then((response) => {
-      showVoteOptions()
-    })
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "content-type": "application/json",
+      "x-api-key": API_KEY,
+    },
+  }).then((response) => {
+    showVoteOptions();
+  });
 }
 
 function showHistoricVotes() {
-
-  document.getElementById('vote-options').style.display = 'none';
-  document.getElementById('vote-results').style.display = 'block';
-  document.getElementById('inner-container').style.display = 'none';
+  document.getElementById("vote-options").style.display = "none";
+  document.getElementById("vote-results").style.display = "block";
+  document.getElementById("inner-container").style.display = "none";
 
   const url = `${API_URL}votes?limit=12&order=DESC`;
 
   fetch(url, {
     headers: {
-      'x-api-key': API_KEY
-    }
+      "x-api-key": API_KEY,
+    },
   })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-
       data.map(function (voteData) {
-
-        const imageData = voteData.image
-        let a = document.createElement('a');
-        let image = document.createElement('img');
+        const imageData = voteData.image;
+        let a = document.createElement("a");
+        let image = document.createElement("img");
         image.setAttribute("class", "display");
         //use the url from the image object
-        a.setAttribute('href', imageData.url);
+        a.setAttribute("href", imageData.url);
         image.src = imageData.url;
 
-        let gridCell = document.createElement('div');
+        let gridCell = document.createElement("div");
 
         if (voteData.value < 0) {
-          gridCell.classList.add('red');
+          gridCell.classList.add("red");
         } else {
-          gridCell.classList.add('green');
+          gridCell.classList.add("green");
         }
 
-        gridCell.classList.add('col-lg2');
+        gridCell.classList.add("col-lg2");
 
         a.appendChild(image);
         gridCell.appendChild(a);
 
-        document.getElementById('grid').appendChild(gridCell);
-
+        document.getElementById("grid").appendChild(gridCell);
       });
-
     })
     .catch(function (error) {
       console.log(error);
     });
-
 }
 
 showVoteOptions();
-
 
 //pop up box
 
